712a318b73e7983259538f21e086623354348be0
Pavel Beletsky <pavel.beletsky@live.com>
2019-10-23 03:13:36 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -10,7 +10,7 @@ def long_planeteer_calls(ar)
   ar.any?{|word| word.length > 4}
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(ar)
+  ar.find{
   cheese_types = ["cheddar", "gouda", "camembert"]
 end
